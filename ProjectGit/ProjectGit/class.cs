c1c20fb33b6b83147c5cf7f759b85fb297f5dc3c
estihool <estihool3767@gmail.com>
2024-05-19 00:51:30 +0300

    succssed

---
+++ ProjectGit/ProjectGit/class.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectGit
+{
+    public class @class
+    {
+        public int newnumber(int number)
+        {
+            int num1=0;
+            num1 += number;
+            return num1;
+        }
+    }
+}
